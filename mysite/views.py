60c783ed1e6a042c1840e548da30cd41d915e41b
mangeshii <thisismangeshi@gmail.com>
2020-07-13 20:16:16 +0530

    Adds simple django project

--- mysite/views.py
+++ mysite/views.py
@@ -7,15 +7,19 @@ def index(request):
 
 
 def analyze(request):
-    djtext = (request.GET.get('text', 'default'))
-    removep = (request.GET.get('removepunc', 'off'))
-    capital = (request.GET.get('allcaps', 'off'))
-    spaceremove = (request.GET.get('removespace', 'off'))
-    newlineremove = (request.GET.get('removenewline', 'off'))
-    charcount = (request.GET.get('countchar', 'off'))
+    # get the text
+    djtext = (request.POST.get('text', 'default'))
 
+    # check checkboxe values
+    removep = (request.POST.get('removepunc', 'off'))
+    capital = (request.POST.get('allcaps', 'off'))
+    spaceremove = (request.POST.get('removespace', 'off'))
+    newlineremove = (request.POST.get('removenewline', 'off'))
+    charcount = (request.POST.get('countchar', 'off'))
+
+    # check which checkbox is on
     if removep == "on":
-        punctuations = ''' !*()[]'";:,<.>/? '''
+        punctuations = ''' !()-[]{};:'"\,<>./?@#$%^&*_~ '''
         analyzed = ""
         for char in djtext:
             if char not in punctuations:
@@ -23,7 +27,6 @@ def analyze(request):
 
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
         djtext = analyzed
-        #return render(request, 'analyze.html', params)
 
     if (capital == "on"):
         analyzed = ""
@@ -32,15 +35,13 @@ def analyze(request):
 
         params = {'purpose': 'Capitalized text', 'analyzed_text': analyzed}
         djtext = analyzed
-        #return render(request, 'analyze.html', params)
 
     if (spaceremove == "on"):
         analyzed = ""
-        analyzed=" ".join(djtext.split()) 
+        analyzed = " ".join(djtext.split())
 
         params = {'purpose': 'extra space remover', 'analyzed_text': analyzed}
         djtext = analyzed
-        #return render(request, 'analyze.html', params)
 
     if (newlineremove == "on"):
         analyzed = ""
@@ -50,20 +51,19 @@ def analyze(request):
 
         params = {'purpose': 'remove extra line', 'analyzed_text': analyzed}
         djtext = analyzed
-        #return render(request, 'analyze.html', params)
 
     if (charcount == "on"):
         analyzed = ""
-
         count = 0
         for char in djtext:
             count += 1
             analyzed = count
 
         params = {'purpose': 'count char', 'analyzed_text': analyzed}
-        #djtext = analyzed
-        #return render(request, 'analyze.html', params)
+        djtext = analyzed
 
+
+    # if none of the checkboxes are on then return error
     if (removep != "on" and capital != "on" and spaceremove != "on" and newlineremove != "on" and charcount != "on"):
         return HttpResponse("Error")
 
